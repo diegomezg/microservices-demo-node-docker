8335a595da2a6e7230beccc02f90c61825073824
Diego Gómez <diego.gomez@digitalonus.com>
2021-05-17 14:11:43 -0500

    Api gateway handlers correction

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -6,9 +6,25 @@ var app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send("Simple API Gateway")
-})
+app.get('/user', (req, res) => {
+    app.get('http://api-users:4041', (req, res) => {
+        res.json({
+            success: true,
+            total,
+            users,
+        }).status(200)
+    });
+});
+
+app.get('/post', (req, res) => {
+    app.get('http://api-posts:4040', (req, res) => {
+        res.json({
+            success: true,
+            total,
+            posts,
+        }).status(200)
+    });
+});
 
 console.log("Simple API Gateway run on localhost:5050")
 
